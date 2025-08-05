39b46063883122142eea9c1cbf996e9494c8393b
arafathsiam99 <siammdarafathislam@gmail.com>
2025-08-05 12:57:09 +0600

    Resume Feedback Page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,7 +1,7 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 
-const ResumeCard = ({resume:{ id, companyName, jobTitle, feedback, imagePath }}:{resume:Resume}) =>{
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
